2bbc378979157b218710a797294620923d2bee81
Priya.Sharma <priyasharma2702@gmail.com>
2024-10-03 23:13:31 +0530

    prepping for login and cart

--- src/feature-components/layout/header/Header.tsx
+++ src/feature-components/layout/header/Header.tsx
@@ -18,6 +18,9 @@ const Header = () => {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
         </ul>
       </nav>
     </header>
